9e52b303029ccde647ae1eee5ff200c682531b3e
luzgiraldo <luzyanithgiraldobedoya@gmail.com>
2023-06-07 17:57:52 -0500

    feat:se sube actualización

--- src/app/pages/itinerario/agregar/agregar.component.ts
+++ src/app/pages/itinerario/agregar/agregar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'ngx-agregar',
@@ -7,15 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  mensaje: string;
+  
+itinerario= {
+  codigo:'',
+  fecha_inicio:'yyyy-MM-dd',
+  hora_inicio:'',
+  fecha_fin:'yyyy-MM-dd',
+  hora_fin:'',
+  longitud:'',
+  visitante:'',
+  guia:'',
+}
 
-  constructor() { }
+
+
+  constructor(public dataService:DataService, private router:Router) { }
 
   ngOnInit(): void {
+    if(this.dataService.itinerarioCurrent){
+      this.itinerario=JSON.parse(JSON.stringify(this.dataService.itineraroCurrent));
+    }
   }
-  imprimir(){
-    alert(this.mensaje),
-    this.mensaje = "El Itinerario fue Regisrado con Exito!";
+  guardar(){
+    let mensaje=''
+    if(this.dataService.itinerarioCurrent){
+      this.dataService.itinerario[this.dataService.itinerarioCurrentPos]=this.itinerario;
+      this.dataService.itinerarioCurrent=null;
+      this.dataService.itinerarioCurrentPos=null;
+      mensaje='Se ha editado el usuario';
+    }else{
+      this.dataService.itinerario.push(this.itinerario);
+      mensaje='Se ha guardado el itineario'
+      alert(JSON.stringify(this.itinerario));
+    }
+    Swal.fire(
+      'Buen trabajo!',
+      mensaje,
+      'success'
+    )
+    this.router.navigate(['/pages/itinerario/listar']);
+    
   }
 
 
